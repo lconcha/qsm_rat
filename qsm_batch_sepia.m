c7ac02e18bf17e90eee5d0addc60f08787193a1f
Luis Concha <lconcha@gmail.com>
2024-02-12 11:10:51 -0600

    first

---
+++ qsm_batch_sepia.m
@@ -0,0 +1,43 @@
+
+addpath('/misc/mansfield/lconcha/software/sepia')
+sepia_addpath;
+
+images_dir = '/misc/lauterbur/lconcha/TMP/qsm/conv';
+
+% General algorithm parameters
+algorParam = struct();
+algorParam.general.isBET = 0 ;
+algorParam.general.isInvert = 0 ;
+algorParam.general.isRefineBrainMask = 0 ;
+% Total field recovery algorithm parameters
+algorParam.unwrap.echoCombMethod = 'Optimum weights' ;
+algorParam.unwrap.unwrapMethod = 'Laplacian (MEDI)' ;
+algorParam.unwrap.isEddyCorrect = 0 ;
+algorParam.unwrap.isSaveUnwrappedEcho = 0 ;
+% Background field removal algorithm parameters
+algorParam.bfr.refine_method = '3D Polynomial' ;
+algorParam.bfr.refine_order = 4 ;
+algorParam.bfr.erode_radius = 0 ;
+algorParam.bfr.erode_before_radius = 0 ;
+algorParam.bfr.method = 'VSHARP' ;
+algorParam.bfr.radius = [10:-1:3] ;
+% QSM algorithm parameters
+algorParam.qsm.reference_tissue = 'None' ;
+algorParam.qsm.method = 'Star-QSM' ;
+algorParam.qsm.padsize     = ones(1,3)*12 ;
+
+
+d = dir(images_dir)
+for r = 3 : length(d)
+    this_rat = d(r).name;
+    % Input/Output filenames
+    input = struct();
+    input = fullfile(images_dir,this_rat);
+    output_basename = fullfile(images_dir,this_rat,'output','sepiabatch');
+    mask_filename = fullfile(images_dir,this_rat,'brain_mask.nii.gz') ;
+  
+    sepiaIO(input,output_basename,mask_filename,algorParam);
+end
+
+
+
